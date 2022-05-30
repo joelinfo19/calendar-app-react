2edfe764207303fe6458531e7605652640263f55
joel <joel_zerg_pro@outlook.com>
2022-05-30 00:13:14 -0500

    main_component_and_start_index

---
+++ src/CalendarApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const CalendarApp=()=>{
+    return (
+        <div>
+            <h1 >
+                calendarAPP
+            </h1>
+        </div>
+    )
+}
