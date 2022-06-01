fd271bd3ea0eae767da8ad2018aca843d541106e
joel <joel_zerg_pro@outlook.com>
2022-05-31 23:59:59 -0500

    changes

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,11 +1,20 @@
 import React from "react";
+import './login.css'
 
 export const LoginScreen=()=>{
     return (
-        <div>
-            <h1 >
-                Login
-            </h1>
+        <div >
+            <form >
+                <div className="form-input">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input className="input-center" type="text"/>
+                </div>
+
+                <div>
+                    <input className="input-center-in" type="text"/>
+                </div>
+
+            </form>
         </div>
     )
 }
