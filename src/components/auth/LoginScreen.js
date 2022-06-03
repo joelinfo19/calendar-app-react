184b8d8c95b87703e3f07234636dce1d421a73b0
joel <joel_zerg_pro@outlook.com>
2022-06-03 00:52:01 -0500

    design_login

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -3,41 +3,41 @@ import './login.css'
 
 export const LoginScreen=()=>{
     return (
-        <div >
-            <form >
-                <div className="form-input">
-                    <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input className="input-center" type="text"/>
+        <div className="form-input-flex">
+            <form className="flex-form">
+                <div className=" ">
+                    <label className="text" htmlFor="exampleInputEmail1">Email address</label>
+                    <input className="box" type="text" />
                 </div>
 
                 <div>
-                    <label htmlFor="exampleInputEmail1">Password</label>
-                    <input className="input-center-in" type="text"/>
+                    <label className="text" htmlFor="exampleInputEmail1">Password</label>
+                    <input className="box" type="text"/>
                 </div>
-
+                <button className="login">
+                    log in
+                </button>
             </form>
-            <button>
-                log in
-            </button>
-            <form >
-                <div className="form-input">
-                    <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input className="input-center" type="text"/>
-                </div>
 
-                <div>
-                    <label htmlFor="exampleInputEmail1">Password</label>
-                    <input className="input-center-in" type="text"/>
-                </div>
-                <div>
-                    <label htmlFor="exampleInputEmail1">Repeat password</label>
-                    <input className="input-center-in" type="text"/>
-                </div>
+            {/*<form >*/}
+            {/*    <div className="form-input">*/}
+            {/*        <label htmlFor="exampleInputEmail1">Email address</label>*/}
+            {/*        <input className="input-center" type="text"/>*/}
+            {/*    </div>*/}
 
-            </form>
-            <button>
-                Register
-            </button>
+            {/*    <div>*/}
+            {/*        <label htmlFor="exampleInputEmail1">Password</label>*/}
+            {/*        <input className="input-center-in" type="text"/>*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*        <label htmlFor="exampleInputEmail1">Repeat password</label>*/}
+            {/*        <input className="input-center-in" type="text"/>*/}
+            {/*    </div>*/}
+
+            {/*</form>*/}
+            {/*<button>*/}
+            {/*    Register*/}
+            {/*</button>*/}
 
         </div>
     )
