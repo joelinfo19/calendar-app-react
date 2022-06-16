3dbaf9ff35cbbe93c2c58af9651e5a6b6073d949
joel <joel_zerg_pro@outlook.com>
2022-06-15 23:35:26 -0500

    react-modal

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,9 +1,40 @@
-import React from "react"
+import React, {useState} from "react"
+import Modal from 'react-modal';
+
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+Modal.setAppElement('#root');
+
 
 export const CalendarModal=()=>{
+    const [isOpen,setIsOpen]=useState(true);
+    const closeModal=()=>{
+        setIsOpen(false)
+    }
     return (
-        <div>
-            WTF
-        </div>
+        <Modal
+            isOpen={isOpen}
+            // onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            style={customStyles}
+            closeTimeoutMS={200}
+            className={"modal"}
+            overlayClassName={" modal-fondo"}
+        >
+            <h1>WTF</h1>
+            <hr/>
+            <span>Hola de nuevo</span>
+
+
+        </Modal>
     )
 }
