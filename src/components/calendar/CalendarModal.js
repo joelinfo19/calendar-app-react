c06168cf7f41369770308aa28c6f145ea432869d
joel <joel_zerg_pro@outlook.com>
2022-06-20 23:53:38 -0500

    update_calendar-Modal

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -16,13 +16,13 @@ Modal.setAppElement('#root');
 
 
 export const CalendarModal=()=>{
-    const [isOpen,setIsOpen]=useState(true);
+    // const [isOpen,setIsOpen]=useState(true);
     const closeModal=()=>{
-        setIsOpen(false)
+        // setIsOpen(false)
     }
     return (
         <Modal
-            isOpen={isOpen}
+            isOpen={true}
             // onAfterOpen={afterOpenModal}
             onRequestClose={closeModal}
             style={customStyles}
@@ -30,9 +30,53 @@ export const CalendarModal=()=>{
             className={"modal"}
             overlayClassName={" modal-fondo"}
         >
-            <h1>WTF</h1>
-            <hr/>
-            <span>Hola de nuevo</span>
+            <h1> Nuevo evento </h1>
+            <hr />
+            <form className="container">
+
+                <div className="form-group mb-2">
+                    <label>Fecha y hora inicio</label>
+                    <input className="form-control" placeholder="Fecha inicio" />
+                </div>
+
+                <div className="form-group mb-2">
+                    <label>Fecha y hora fin</label>
+                    <input className="form-control" placeholder="Fecha inicio" />
+                </div>
+
+                <hr />
+                <div className="form-group mb-2">
+                    <label>Titulo y notas</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Título del evento"
+                        name="title"
+                        autoComplete="off"
+                    />
+                    <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+                </div>
+
+                <div className="form-group mb-2">
+                <textarea
+                    type="text"
+                    className="form-control"
+                    placeholder="Notas"
+                    rows="5"
+                    name="notes"
+                ></textarea>
+                    <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+                </div>
+
+                <button
+                    type="submit"
+                    className="btn btn-outline-primary btn-block"
+                >
+                    <i className="far fa-save"></i>
+                    <span> Guardar</span>
+                </button>
+
+            </form>
 
 
         </Modal>
